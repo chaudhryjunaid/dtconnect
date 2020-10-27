29b601861b62d2b114d225700b575caef7133bd7
Chaudhry Junaid Anwar <junaidanwar10@gmail.com>
2020-10-27 17:41:36 +0500

    more insertDoc work

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const url = process.env.URL;
 
 console.log('Creating SOAP client for: ', process.env.URL);
-soap.createClient(url, { endpoint: uri }, function(err, client) {
+soap.createClient(url, { endpoint: url }, function(err, client) {
     if (err) {
         return console.log('Error creating SOAP client:', err.message, err.stack);
     }
