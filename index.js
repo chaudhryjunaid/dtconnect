178c331392a2b7d313b5441e0ba4f3aaaefd77c6
Chaudhry Junaid Anwar <junaidanwar10@gmail.com>
2020-10-27 15:58:07 +0500

    initial soap code

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 require('dotenv').config();
+const soap = require('soap');
+const url = process.env.URL;
+// const args = {name: 'value'};
 
-console.log(process.env.URL);
+console.log('Creating SOAP client for: ', process.env.URL);
+soap.createClient(url, function(err, client) {
+    if (err) {
+        return console.log('Error creating SOAP client:', err.message, err.stack);
+    }
+    console.log('Success creating SOAP client!');
+    // client.MyFunction(args, function(err, result) {
+    //     console.log(result);
+    // });
+});
