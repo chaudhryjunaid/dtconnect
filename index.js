d9bc4527c0aed6dc4606fcd70092f4da4152a0f1
Chaudhry Junaid Anwar <junaidanwar10@gmail.com>
2020-10-27 17:09:40 +0500

    more insertDoc work

--- index.js
+++ index.js
@@ -21,11 +21,11 @@ soap.createClient(url, function(err, client) {
         console.log('Register result:', result);
         const cookieValue = parseInt(result.RegisterResult, 10);
         console.log('clientId:', cookieValue);
-        fs.readFile('sum.pdf', null, function(err, content) {
+        fs.readFile('sum.pdf', {encoding: null, flag: 'rb'}, function(err, content) {
             if (err) {
                 return console.log('Error reading file:', err.message, err.stack);
             }
-            console.log('typeof(content):', typeof content);
+            console.log('isBuffer(content):', Buffer.isBuffer(content));
             const insertArgs = {
                 clientId: cookieValue,
                 sourceName: 'Oak Park',
