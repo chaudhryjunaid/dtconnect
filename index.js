9e6d7f5fa7966e1b41244642d3f2dbd14926f2fe
Chaudhry Junaid Anwar <junaidanwar10@gmail.com>
2021-01-13 14:00:51 +0500

    comment calls

--- index.js
+++ index.js
@@ -30,37 +30,37 @@ soap.createClient(url, { endpoint: url }, function(err, client) {
     console.log('Success creating SOAP client!');
     console.log(util.inspect(client.describe(), false, null, true /* enable colors */));
 
-    const args = {
-        clientType: process.env.IRC,
-        userName: process.env.USERNAME,
-        clientIPAddress: null
-    };
-    client.Register(args, function(err, result) {
-        if (err) {
-            return console.log('Error registering client:', err.message, err.stack);
-        }
-        console.log('Register result:', result);
-        const clientId = result.RegisterResult;
-        console.log('clientId:', clientId);
-        fs.readFile('sum.pdf', {encoding: 'base64'}, function(err, content) {
-            if (err) {
-                return console.log('Error reading file:', err.message, err.stack);
-            }
-            console.log('isBuffer(content):', Buffer.isBuffer(content));
-            const insertArgs = {
-                clientId,
-                sourceName: process.env.BUC,
-                sourceId: process.env.BUC,
-                xmlAssociationDoc: getXmlAssocDoc(),
-                mimeType: 'application/pdf',
-                documentFile: content
-            };
-            client.InsertDocument(insertArgs, function(err, insertResult) {
-                if (err) {
-                    return console.log(err, 'Error in InsertDocument:', err.message, err.stack);
-                }
-                console.log('InsertDocument result:', insertResult);
-            });
-        });
-    });
+    // const args = {
+    //     clientType: process.env.IRC,
+    //     userName: process.env.USERNAME,
+    //     clientIPAddress: null
+    // };
+    // client.Register(args, function(err, result) {
+    //     if (err) {
+    //         return console.log('Error registering client:', err.message, err.stack);
+    //     }
+    //     console.log('Register result:', result);
+    //     const clientId = result.RegisterResult;
+    //     console.log('clientId:', clientId);
+    //     fs.readFile('sum.pdf', {encoding: 'base64'}, function(err, content) {
+    //         if (err) {
+    //             return console.log('Error reading file:', err.message, err.stack);
+    //         }
+    //         console.log('isBuffer(content):', Buffer.isBuffer(content));
+    //         const insertArgs = {
+    //             clientId,
+    //             sourceName: process.env.BUC,
+    //             sourceId: process.env.BUC,
+    //             xmlAssociationDoc: getXmlAssocDoc(),
+    //             mimeType: 'application/pdf',
+    //             documentFile: content
+    //         };
+    //         client.InsertDocument(insertArgs, function(err, insertResult) {
+    //             if (err) {
+    //                 return console.log(err, 'Error in InsertDocument:', err.message, err.stack);
+    //             }
+    //             console.log('InsertDocument result:', insertResult);
+    //         });
+    //     });
+    // });
 });
