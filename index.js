599558b0f7385f421c97f697b8b090fb60ab5c36
Chaudhry Junaid Anwar <junaidanwar10@gmail.com>
2021-01-13 15:16:34 +0500

    fix bug

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ const getXmlAssocDoc = () => {
 const getXml = (xmlObj) => {
     const doc = create(xmlObj);
     const xml = doc.end({ prettyPrint: true });
+    console.log('##\n', xml);
     return xml;
 };
 
